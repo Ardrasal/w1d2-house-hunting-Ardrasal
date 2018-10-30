0f25b98dad4e63fd0685a62642c572fa75e0aeca
Ardra Salemy <ardrasalemy@gmail.com>
2018-10-30 19:27:27 -0400

    This was hard but I got it!

---
+++ House_Hunting.py
@@ -0,0 +1,30 @@
+monthly_salary = int(input("Enter your annual salary: ")) / 12
+portion_saved = float(input("Enter the percent of your salary to save, as a decimal: "))
+total_cost = int(input("Enter the cost of your dream home: "))
+# total_cost = int(total_cost)
+# print(total_cost)
+portion_down_payment = float(0.25 * total_cost)
+# print(portion_down_payment)
+current_savings = int(0)
+r = float(0.04)
+annual_return = current_savings * r/12
+# print(annual_return)
+months = int(0)
+
+
+# (current_savings * 0.04)/12)
+while (current_savings <= portion_down_payment):
+    #calculate interest
+    interest = float(current_savings * 0.04)/12
+    #add portion_saved
+    monthly_saved = portion_saved * monthly_salary
+    #add interest + portion_saved + current savings
+    current_savings = interest + monthly_saved + current_savings
+
+    months = months + 1
+# print when months stop increasing by 1/print months when conditions are met--stuck here!!
+# If (months = )
+    
+
+print("Number of months: " + str(months))
+
